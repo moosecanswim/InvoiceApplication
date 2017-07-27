09bac1233f6238053d6b6fd673c36218da379962
moosecanswim <kyle.gallagher.09@gmail.com>
2017-07-27 17:49:33 -0400

    worked on the program a bunch  works but will need to catch exceptions

--- src/invoiceApplication/InvoiceApplication.java
+++ src/invoiceApplication/InvoiceApplication.java
@@ -1,10 +1,58 @@
 package invoiceApplication;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class InvoiceApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<Item> itemList = new ArrayList();
+		double totalTax=0.0;
+		double totalDue = 0.0;
 
+		Scanner scan = new Scanner (System.in);
+		System.out.println("First, tell me what the tax rate is where you ate.  tell me as a %");
+		double inTaxRate = scan.nextDouble();
+
+
+		String resp = "y";
+		do{
+			System.out.println("Please enter a product description: ");
+			String itemDescription = scan.next();
+			System.out.println("Please enter a product price: ");
+			double itemPrice = scan.nextDouble();
+
+			double taxDue = (inTaxRate/100)*itemPrice;
+			
+			
+
+			Item newItem = new Item(itemDescription, itemPrice, taxDue);
+			newItem.setTotalDue(newItem.taxAndPrice());
+			itemList.add(newItem);
+			
+
+			System.out.println("Would you like to add another item? (y/n)");
+			resp = scan.next();
+
+
+		}while(resp.equalsIgnoreCase("y"));
+
+		System.out.println("I have " + itemList.size() + " Products");
+		for(Item i: itemList){
+			totalTax = totalTax+i.getTaxDue();
+			totalDue = totalDue +i.getTotalDue();
+
+			System.out.println();
+
+			//this is where we will print all the outputs
+			i.printItem();
+		}
+		System.out.println(String.format("Total Tax: $%.2f" , totalTax));
+		System.out.println(String.format("Total Ammount: $%.2f" , totalDue));
+
+
+		System.out.println("Program Exiting");	
 	}
+
 
 }
--- src/invoiceApplication/Item.java
+++ src/invoiceApplication/Item.java
@@ -1,5 +1,61 @@
 package invoiceApplication;
 
 public class Item {
+	
+	private double itemPrice;
+	private String itemDescription;
+	private double taxDue;
+	private double totalDue;
+	
+	public double getTaxDue() {
+		return taxDue;
+	}
 
+	public void setTaxDue(double taxDue) {
+		this.taxDue = taxDue;
+	}
+
+	public Item(String itemDescription, double itemPrice, double taxDue){
+		
+		this.itemDescription = itemDescription;
+		this.itemPrice = itemPrice;
+		this.taxDue = taxDue;
+	}
+
+	public double getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(double itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+	public String getItemDescription() {
+		return itemDescription;
+	}
+
+	public void setItemDescription(String itemDescription) {
+		this.itemDescription = itemDescription;
+	}
+
+	public double getTotalDue() {
+		return totalDue;
+	}
+	public void setTotalDue(double input){
+		this.totalDue = input;
+	}
+	public double taxAndPrice(){
+		return taxDue+itemPrice;
+	}
+	
+	
+	public void printItem(){
+		String output = String.format("Item Description: %s | Price: $%.2f  | tax: $%.2f",itemDescription,itemPrice,taxDue);
+		System.out.println(output);
+		System.out.println();
+	}
+
+	
+	
+	
 }
