3fbf515dfa6beb1fdfbb86452eb5163d7c153606
moosecanswim <kyle.gallagher.09@gmail.com>
2017-07-27 16:46:11 -0400

    first commit

---
+++ src/invoiceApplication/InvoiceApplication.java
@@ -0,0 +1,10 @@
+package invoiceApplication;
+
+public class InvoiceApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/invoiceApplication/Item.java
@@ -0,0 +1,5 @@
+package invoiceApplication;
+
+public class Item {
+
+}
