d35491ff9339fffffa617db45d0ee0a6fa10ef7b
moosecanswim <kyle.gallagher.09@gmail.com>
2017-07-28 10:20:18 -0400

    added the ability to catch entery errors

--- src/invoiceApplication/InvoiceApplication.java
+++ src/invoiceApplication/InvoiceApplication.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InvoiceApplication {
-	
-	
+
+
 	/***
 	 * This application will ask the user for the going tax rate
 	 * Then prompt the user for an item description and price.
@@ -14,7 +14,7 @@ public class InvoiceApplication {
 	 * 
 	 * @param args
 	 */
-	
+
 	public static void main(String[] args) {
 		ArrayList<Item> itemList = new ArrayList();
 		double totalTax=0.0;
@@ -22,51 +22,79 @@ public class InvoiceApplication {
 		Scanner scan = new Scanner (System.in);
 		int itemCount = 0;
 		String resp = "y";
-		
-		//prompt the user for the tax rate and accept it
+		double taxRate = 0.0;
+
+		//prompt the user for the tax rate and accept it (only accepts a double otherwise it re-prompts you for another tax rate
+
 		System.out.println("First, tell me what the going tax rate is for this sale.  tell me as a %");
-		double inTaxRate = scan.nextDouble();
-		
+		Boolean tempTry = true;
+		while(tempTry){
+			tempTry = true;
+			
+			if (scan.hasNextDouble()){
+				double inTaxRate = scan.nextDouble();
+				taxRate = inTaxRate/100;
+				tempTry = false;
+			}
+			else{
+				System.out.println("You did not enter a valid tax rate.  Please enter a tax rate ex: (6.01 or 5 or .77).  Try Again: ");
+				scan.next();			
+			}
+		}
+
 		//keep asking for item descriptions and prices until the user says they dont want to continue
 		do{
 			itemCount++;
 			String itemNumber = String.valueOf(itemCount);
 			System.out.println("Please enter a product description: ");
 			String itemDescription = scan.next();
-			System.out.println("Please enter a product price: ");
-			double itemPrice = scan.nextDouble();
 
-			double taxDue = (inTaxRate/100)*itemPrice;
-			
-			
+			//checks to make sure it is a double price
+			System.out.println("Please enter a product price: ");
+			double itemPrice = 0.0;
+			boolean tempTry2=true;
+			while (tempTry2){
+				if(scan.hasNextInt()){
+				double tempItemPrice = scan.nextDouble();
+				itemPrice = tempItemPrice;
+				tempTry2 = false;
+				}
+				else{
+					System.out.println("You need to enter an actuall price for the item.  Try Again: ");
+					scan.next();
+				}
+
+			}
+
+			double taxDue = (taxRate)*itemPrice;
 
 			Item newItem = new Item(itemCount,itemDescription, itemPrice, taxDue);
 			newItem.setTotalDue(newItem.taxAndPrice());
 			itemList.add(newItem);
-			
 
-			System.out.println("Would you like to add another item? (y/n)");
+
+			System.out.println("Would you like to add another item? (y)es or (n)o");
 			resp = scan.next();
 
 
 		}while(resp.equalsIgnoreCase("y"));
 
-		
+
 		//OUTPUT: go through the arraylist of Item s and print them out
 		System.out.println("Invoice for " + itemList.size() + " Products");
 		for(Item i: itemList){
-			
+
 			totalTax = totalTax+i.getTaxDue();
 			totalDue = totalDue +i.getTotalDue();
 
 			i.printItem();
-			
+
 		}
 		//print the totals for tax due and total ammount due
 		System.out.println();
 		System.out.println("-----------------------------------");
-		System.out.println("Tax rate: " + inTaxRate + "%");
-		System.out.println(String.format("Total Charged: $%.2f" , totalTax));
+		System.out.println("Tax rate: " + taxRate*100 + "%");
+		System.out.println(String.format("Tax Charged: $%.2f" , totalTax));
 		System.out.println(String.format("Total Ammount Due: $%.2f" , totalDue));
 
 
